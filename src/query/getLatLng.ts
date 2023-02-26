017db177f9fc7eb1423e16a3e2668b7179ca2952
seongminn <berry9901@gmail.com>
2023-02-26 18:02:33 +0900

    refactor(query): 위/경도 반환 타입 변경 및 타입 지정

--- src/query/getLatLng.ts
+++ src/query/getLatLng.ts
@@ -7,13 +7,13 @@ const getLatLng = ({ line }: { line: number }) => {
   const { data: latLngData } = useQuery(QueryKey.LAT_LNG, () =>
     fetcher({ path: URLs.LAT_LNG, page: 1, perPage: 2000 })
   );
-  let subwayObj: { [key: string]: number[] } = {};
 
-  latLngData?.data
+  return latLngData?.data
     .filter((obj: any) => obj.호선 === line)
-    .map((item: any) => (subwayObj[`${item.역명}역`] = [item.위도, item.경도]));
-
-  return subwayObj;
+    .map((item: any) => ({
+      name: `${item.역명}역`,
+      position: { lat: Number(item.위도), lng: Number(item.경도) },
+    }));
 };
 
 export default getLatLng;
