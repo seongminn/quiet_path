1032fb3c6b276e0bd643f3029c2d9cb1830af1dc
seongminn <berry9901@gmail.com>
2023-02-26 16:29:12 +0900

    refactor(getLatLng): 호선별 위/경도 반환 함수의 반환 형식 변경

--- src/query/getLatLng.ts
+++ src/query/getLatLng.ts
@@ -7,10 +7,13 @@ const getLatLng = ({ line }: { line: number }) => {
   const { data: latLngData } = useQuery(QueryKey.LAT_LNG, () =>
     fetcher({ path: URLs.LAT_LNG, page: 1, perPage: 2000 })
   );
+  let subwayObj: { [key: string]: number[] } = {};
 
-  return latLngData?.data
+  latLngData?.data
     .filter((obj: any) => obj.호선 === line)
-    .map((item: any) => [`${item.역명}역`, item.위도, item.경도]);
+    .map((item: any) => (subwayObj[`${item.역명}역`] = [item.위도, item.경도]));
+
+  return subwayObj;
 };
 
 export default getLatLng;
