5ac68ac9326730ef7c7123a67da6520e43c930fe
seongminn <berry9901@gmail.com>
2023-02-26 19:03:22 +0900

    feat(InfoBox): 마커 클릭 시 InfoBox 생성

--- src/components/Map/Map.tsx
+++ src/components/Map/Map.tsx
@@ -1,23 +1,33 @@
+import tw from 'twin.macro';
 import { mapOptions, mapStyle } from '@/config/mapConfig';
 import { useGeolocation } from '@/hook';
 import { SubwayPosition } from '@/types/subway/Congestion';
-import { LoadScript, GoogleMap, MarkerF } from '@react-google-maps/api';
-import tw from 'twin.macro';
-
-const { VITE_GOOGLE_API_KEY } = import.meta.env;
+import {
+  LoadScript,
+  GoogleMap,
+  MarkerF,
+  InfoBoxF,
+} from '@react-google-maps/api';
+import { useState } from 'react';
 
 const Wrapper = tw.div`
-  max-w-[60%] min-w-[360px] h-full rounded-10
-  shadow-gray-300 drop-shadow-lg
+max-w-[60%] min-w-[360px] h-full rounded-10
+shadow-gray-300 drop-shadow-lg
   flex-center
 
   laptop:(w-2/3 max-w-[50%])
 
   mobile:(w-full)
-`;
+  `;
 
 const Map = ({ subwayList }: { subwayList: SubwayPosition[] }) => {
+  const { VITE_GOOGLE_API_KEY } = import.meta.env;
   const { message, coordinates } = useGeolocation();
+  const [isShown, toggleShown] = useState<boolean>(false);
+
+  const handleToggle = () => {
+    toggleShown((prev) => !prev);
+  };
 
   return (
     <Wrapper>
@@ -28,8 +38,14 @@ const Map = ({ subwayList }: { subwayList: SubwayPosition[] }) => {
           mapContainerStyle={mapStyle}
           options={{ mapTypeControl: false, styles: mapOptions }}
         >
-          {subwayList?.map((item) => (
-            <MarkerF position={item.position} key={item.name} />
+          {subwayList?.map(({ position, name }) => (
+            <MarkerF position={position} key={name} onClick={handleToggle}>
+              {isShown && (
+                <InfoBoxF onCloseClick={handleToggle}>
+                  <div>hi</div>
+                </InfoBoxF>
+              )}
+            </MarkerF>
           ))}
         </GoogleMap>
       </LoadScript>
