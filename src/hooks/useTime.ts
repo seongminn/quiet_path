14af9b467562a01671f715630c054deb8ed829da
seongminn <berry9901@gmail.com>
2023-03-01 14:06:21 +0900

    feat(time): 30분 단위의 시간 정보 제공 훅 생성

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,7 +1,23 @@
+const padZero = (time: number) => {
+  return time.toString().padStart(2, '0');
+  // return ('0' + time).slice(-2); 뭐가 더 효율적일까?
+};
+
 const useTime = () => {
   const date = new Date();
-  console.log(date);
-  return null;
+  let hour = date.getHours();
+  let minute = date.getMinutes();
+
+  if (minute >= 45) {
+    minute = 0;
+    hour++;
+  } else if (minute >= 15) {
+    minute = 30;
+  } else {
+    minute = 0;
+  }
+
+  return `${padZero(hour)}시${padZero(minute)}분`;
 };
 
 export default useTime;
