9cb7702025d0d498ace556a861ac328de5e2a346
seongminn <berry9901@gmail.com>
2023-03-01 15:11:59 +0900

    feat(InfoBox): 현재 시간에 따른 InfoBox 내용 구성
    시간을 30분 단위로 나누고, 현재 시간과 가장 가까운 지점의 지하철 혼잡도를 출력합니다.

--- src/types/subway.ts
+++ src/types/subway.ts
@@ -1,3 +1,42 @@
+export type Clocks =
+  | '5시30분'
+  | '6시00분'
+  | '6시30분'
+  | '7시00분'
+  | '7시30분'
+  | '8시00분'
+  | '8시30분'
+  | '9시00분'
+  | '9시30분'
+  | '10시00분'
+  | '10시30분'
+  | '11시00분'
+  | '11시30분'
+  | '12시00분'
+  | '12시30분'
+  | '13시00분'
+  | '13시30분'
+  | '14시00분'
+  | '14시30분'
+  | '15시00분'
+  | '15시30분'
+  | '16시00분'
+  | '16시30분'
+  | '17시00분'
+  | '17시30분'
+  | '18시00분'
+  | '18시30분'
+  | '19시00분'
+  | '19시30분'
+  | '20시00분'
+  | '20시30분'
+  | '21시00분'
+  | '21시30분'
+  | '22시00분'
+  | '22시30분'
+  | '23시00분'
+  | '23시30분';
+
 export type SubwayCongestion = {
   연번: number;
   조사일자: string;
@@ -5,43 +44,8 @@ export type SubwayCongestion = {
   역번호: number;
   역명: string;
   구분: string;
-  '5시30분': string;
-  '6시00분': string;
-  '6시30분': string;
-  '7시00분': string;
-  '7시30분': string;
-  '8시00분': string;
-  '8시30분': string;
-  '9시00분': string;
-  '9시30분': string;
-  '10시00분': string;
-  '10시30분': string;
-  '11시00분': string;
-  '11시30분': string;
-  '12시00분': string;
-  '12시30분': string;
-  '13시00분': string;
-  '13시30분': string;
-  '14시00분': string;
-  '14시30분': string;
-  '15시00분': string;
-  '15시30분': string;
-  '16시00분': string;
-  '16시30분': string;
-  '17시00분': string;
-  '17시30분': string;
-  '18시00분': string;
-  '18시30분': string;
-  '19시00분': string;
-  '19시30분': string;
-  '20시00분': string;
-  '20시30분': string;
-  '21시00분': string;
-  '21시30분': string;
-  '22시00분': string;
-  '22시30분': string;
-  '23시00분': string;
-  '23시30분': string;
+} & {
+  [key in Clocks as string]: string;
 };
 
 export type ResponseProps = {
