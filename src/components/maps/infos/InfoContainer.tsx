6755f85c01bdc0e6fcad8cec94663729fc5494d2
seongminn <berry9901@gmail.com>
2023-03-01 23:57:10 +0900

    feat(InfoBox): 스켈레톤 코드 생성

--- src/components/maps/infos/InfoContainer.tsx
+++ src/components/maps/infos/InfoContainer.tsx
@@ -11,12 +11,24 @@ const InfoContainer = ({ line, name }: { line: number; name: string }) => {
   });
   const initial = useTime();
 
-  if (!congestion) return null;
+  if (!congestion)
+    return (
+      <div css={tw`bg-white w-full animate-pulse`}>
+        <div css={tw`h-15 w-1/2 rounded-2 bg-gray-400 mb-5`}></div>
+        <div css={tw`flex justify-between items-center mb-2`}>
+          <span css={tw`h-8 w-full rounded-2 bg-gray-400`}></span>
+          {/* <span css={tw`h-8 w-3/12 rounded-2 bg-gray-400`}></span> */}
+        </div>
+        <div css={tw`flex justify-between items-center mb-2`}>
+          <span css={tw`h-8 w-8/12 rounded-2 bg-gray-400`}></span>
+          <span css={tw`h-8 w-3/12 rounded-2 bg-gray-400`}></span>
+        </div>
+      </div>
+    );
 
   return (
     <div css={tw`bg-white w-full`}>
       <h6 css={tw`mb-5`}>{name}</h6>
-
       {congestion.map((obj) => (
         <Info
           key={`${line}${name}${obj.구분}`}
