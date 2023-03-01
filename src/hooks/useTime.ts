a90f455b627a4ec9323eef776d6facd4275f5180
seongminn <berry9901@gmail.com>
2023-03-01 15:09:56 +0900

    refactor(useTime): 불필요한 리렌더링 방지

--- src/hooks/useTime.ts
+++ src/hooks/useTime.ts
@@ -1,12 +1,14 @@
-const padZero = (time: number) => {
-  return time.toString().padStart(2, '0');
-  // return ('0' + time).slice(-2); 뭐가 더 효율적일까?
-};
+import { useCallback, useMemo } from 'react';
 
 const useTime = () => {
-  const date = new Date();
-  let hour = date.getHours();
-  let minute = date.getMinutes();
+  const getTimeString = useCallback((time: number) => {
+    return time.toString().padStart(2, '0');
+    // return ('0' + time).slice(-2); 뭐가 더 효율적일까?
+  }, []);
+
+  const now = useMemo(() => new Date(), []);
+  let hour = now.getHours();
+  let minute = now.getMinutes();
 
   if (minute >= 45) {
     minute = 0;
@@ -17,7 +19,13 @@ const useTime = () => {
     minute = 0;
   }
 
-  return `${padZero(hour)}시${padZero(minute)}분`;
+  const hourString = useMemo(() => getTimeString(hour), [getTimeString, hour]);
+  const minuteString = useMemo(
+    () => getTimeString(minute),
+    [getTimeString, minute]
+  );
+
+  return `${hourString}시${minuteString}분`;
 };
 
 export default useTime;
