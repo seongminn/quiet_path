a713ad43436d9d706b96f32c2f2cc69949d6e9aa
seongminn <berry9901@gmail.com>
2023-03-04 16:17:00 +0900

    refactor(gnb): 스타일 수정 및 컴포넌트 구조 변경

--- src/components/common/Gnb/Menu.tsx
+++ src/components/common/Gnb/Menu.tsx
@@ -1,4 +1,7 @@
 import SvgIcon from '@/components/common/Icon/SvgIcon';
+import { ReactComponent as DownIcon } from '@/assets/icons/icon-chevron-down.svg';
+import { ReactNode } from 'react';
+import tw from 'twin.macro';
 
 const Menu = ({
   icon,
@@ -6,14 +9,28 @@ const Menu = ({
   height,
   strokeWidth = 1.2,
   styles,
+  title,
+  children,
 }: {
   icon: string;
   width: number;
   height: number;
   strokeWidth?: number;
+  title?: string;
   styles?: any;
+  children?: ReactNode;
 }) => {
-  return <div css={styles}>{SvgIcon[icon](width, height, strokeWidth)}</div>;
+  return (
+    <div css={[tw`flex justify-between items-center w-full`]}>
+      <div css={tw`flex justify-center items-center`}>
+        <div css={[styles]}>{SvgIcon[icon](width, height, strokeWidth)}</div>
+        <p css={tw`p-10`}>{title}</p>
+      </div>
+      {children && (
+        <DownIcon width={12} height={12} css={tw`p-0 hover:(bg-current)`} />
+      )}
+    </div>
+  );
 };
 
 export default Menu;
