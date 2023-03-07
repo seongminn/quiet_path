143f3318e2208e6d68382d79359cfe6fd2142f87
seongminn <berry9901@gmail.com>
2023-03-08 00:44:10 +0900

    refactor(api): 지하철 노선 각각에 대한 데이터를 제공하는 api로 변경

--- src/api/fetcher.ts
+++ src/api/fetcher.ts
@@ -2,20 +2,8 @@ import axios from 'axios';
 
 const { VITE_API_KEY, VITE_BASE_URL } = import.meta.env;
 
-export const fetcher = async ({
-  path,
-  page,
-  perPage,
-}: {
-  path: string;
-  page?: number;
-  perPage?: number;
-}) => {
-  let URL;
-
-  if (!(page || perPage)) URL = path;
-  else
-    URL = `${VITE_BASE_URL}${path}?page=${page}&perPage=${perPage}&serviceKey=${VITE_API_KEY}`;
+export const fetcher = async ({ path }: { path: string }) => {
+  const URL = `${VITE_BASE_URL}${path}?page=${1}&perPage=${1000}&serviceKey=${VITE_API_KEY}`;
 
   try {
     const { data } = await axios.get(URL);
