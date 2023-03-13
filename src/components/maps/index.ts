89a4fc095c1f04b8f330e33e0ac9b456bee6ccd1
seongminn <berry9901@gmail.com>
2023-03-13 16:09:24 +0900

    refactor(index): 모듈을 디렉토리 별 index.ts에서 일괄적으로 내보내기

--- src/components/maps/index.ts
+++ src/components/maps/index.ts
@@ -1,4 +1,3 @@
 export { default as MapContainer } from './MapContainer';
 export { default as Map } from './Map';
 export { default as Marker } from './Marker';
-export { default as InfoContainer } from './infos/InfoContainer';
