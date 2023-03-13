f1c99ff0523dd0cf3adffae8ae3d3d279267a9ec
seongminn <berry9901@gmail.com>
2023-03-13 14:01:30 +0900

    fix(useMemo): 자바스크립트의 gc로 변수를 초기화하기 때문에 useMemo 삭제

--- src/hooks/useDayOfWeek.ts
+++ src/hooks/useDayOfWeek.ts
@@ -1,10 +1,5 @@
-import { useMemo } from 'react';
-
 const getDayOfWeek = (date: Date) => {
-  const weekdays = useMemo(
-    () => ['일요일', '평일', '평일', '평일', '평일', '평일', '토요일'],
-    []
-  );
+  const weekdays = ['일요일', '평일', '평일', '평일', '평일', '평일', '토요일'];
   const dayOfWeek = (date as Date).getDay();
 
   return weekdays[dayOfWeek];
