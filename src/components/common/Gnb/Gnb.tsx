fdfb610d6f640b7899ef9c3009ba5c5b04ee1912
seongminn <berry9901@gmail.com>
2023-03-16 01:11:40 +0900

    refactor(icon): 아이콘 절대 경로로 수정

--- src/components/common/Gnb/Gnb.tsx
+++ src/components/common/Gnb/Gnb.tsx
@@ -1,7 +1,7 @@
-import { useCallback, useState } from 'react';
+import { useState } from 'react';
 import tw, { styled } from 'twin.macro';
-import GnbLayout from './GnbLayout';
-import Menu from './Menu';
+import { Menu, Layout } from '.';
+import LogoSrc from '@/assets/icons/icon-logo.png';
 
 const List = styled.div<{ open: boolean }>(({ open }) => [
   tw`px-10 my-20 flex flex-col gap-5`,
@@ -15,9 +15,9 @@ const Gnb = () => {
   const [open, setOpen] = useState<boolean>(false);
 
   return (
-    <GnbLayout open={open} setOpen={setOpen}>
+    <Layout open={open} setOpen={setOpen}>
       <div css={tw`flex justify-start items-center`}>
-        <img src='src/assets/icons/icon-logo.png' css={tw`m-16`} alt='logo' />
+        <img src={LogoSrc} css={tw`m-16`} alt='logo' />
       </div>
       <List open={open}>
         <ul>
@@ -32,7 +32,7 @@ const Gnb = () => {
           </li>
         </ul>
       </List>
-    </GnbLayout>
+    </Layout>
   );
 };
 
