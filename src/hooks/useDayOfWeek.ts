6e81bfb457e4e2a2e3be49667f2dedc5834c4d49
seongminn <berry9901@gmail.com>
2023-03-20 15:18:01 +0900

    fix: 공공 API 변경에 따라 요일 params의 주말을 모두 "토요일"로 변경

--- src/hooks/useDayOfWeek.ts
+++ src/hooks/useDayOfWeek.ts
@@ -1,5 +1,5 @@
 const getDayOfWeek = (date: Date) => {
-  const weekdays = ['일요일', '평일', '평일', '평일', '평일', '평일', '토요일'];
+  const weekdays = ['토요일', '평일', '평일', '평일', '평일', '평일', '토요일'];
   const dayOfWeek = (date as Date).getDay();
 
   return weekdays[dayOfWeek];
