f23ab41de5eef1bfeb70cc863a4b43b7cb9be6ec
seongminn <berry9901@gmail.com>
2023-03-22 01:59:35 +0900

    fix: 바텀 시트에 스크롤이 적용되지 않던 버그 수정

--- src/components/subway/SubwayList.tsx
+++ src/components/subway/SubwayList.tsx
@@ -6,14 +6,14 @@ import { useRecoilState, useRecoilValue } from 'recoil';
 import { subwayListState } from '@/recoil/atoms/subwayListState';
 
 const Container = styled.section([
-  tw`w-full h-auto relative`,
+  tw`w-full h-auto`,
   tw`laptop:(flex flex-col justify-start items-start min-w-[150px] max-w-[350px] w-full h-full rounded-10 mx-20)`,
 ]);
 
 const List = tw.div`
-py-10 pr-20 w-full 
+py-10 pr-20 w-full not-last:(mb-20)
 relative not-last:after:(content-[''] w-50 h-1 bg-gray-400 absolute bottom-[-10px] left-0)
-first-of-type:pt-0 last-of-type:pb-0
+laptop:(first-of-type:pt-0 last-of-type:pb-0)
 `;
 
 const SubwayList = ({
@@ -33,26 +33,30 @@ const SubwayList = ({
       <div
         css={[
           tw`flex flex-col justify-start`,
-          tw`overflow-y-scroll scrollbar scrollbar-thumb-gray-600 scrollbar-w-4 scrollbar-thumb-rounded-2 scrollbar-track-transparent`,
-          tw`absolute transition-all w-full left-0 rounded-t-10 z-50`,
+          tw`scrollbar scrollbar-thumb-gray-600 scrollbar-w-4 scrollbar-thumb-rounded-2 scrollbar-track-transparent`,
+          tw`absolute transition-all w-full bottom-0 left-0 rounded-t-10 z-50`,
 
-          clicked ? tw`top-[200px] bg-white` : tw`top-[1000px]`,
+          clicked ? tw`bg-white h-[600px]` : tw`h-50 opacity-[0.01]`,
 
-          tw`laptop:(static flex flex-col justify-start gap-20 h-full w-full rounded-5)`,
+          tw`laptop:(static flex flex-col justify-start gap-20 h-full w-full rounded-5 opacity-100 overflow-y-scroll mt-10)`,
         ]}
       >
         <div
-          tw='p-4 h-50 cursor-pointer hover:bg-gray-100'
+          tw='py-25 h-50 cursor-pointer hover:bg-gray-100 laptop:hidden'
           data-drawer-toggle='drawer-swipe'
           onClick={() => setClicked((prev) => !prev)}
         >
           <span tw='absolute w-30 h-4 -translate-x-1/2 bg-gray-400 rounded-lg top-10 left-1/2 dark:bg-gray-600'></span>
         </div>
-        {subwayList?.map((item) => (
-          <List key={line + item.name}>
-            <InfoContainer name={item.name} />
-          </List>
-        ))}
+        <div
+          css={tw`overflow-y-scroll scrollbar scrollbar-thumb-gray-600 scrollbar-w-4 scrollbar-thumb-rounded-2 scrollbar-track-transparent px-20 laptop:pl-0`}
+        >
+          {subwayList?.map((item) => (
+            <List key={line + item.name}>
+              <InfoContainer name={item.name} />
+            </List>
+          ))}
+        </div>
       </div>
     </Container>
   );
